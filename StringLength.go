61145ea3095fbf2c54fa77f159c5d6d0a77bc87b
1234github <irumraufsatti@hotmail.com>
2016-03-09 15:20:54 +0500

    Package of String Length

---
+++ StringLength.go
@@ -0,0 +1,8 @@
+// Package StringLength is used to find out the length of a string
+package StringLength
+
+// Length function find out the length of string
+func Length(variable string) int {
+
+	return len(variable)
+}
